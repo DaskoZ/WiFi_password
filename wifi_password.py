8287fa8ef56d1a0471b04ac3ed79ef33b83ea715
DaskoZ <osama.ahmad1998@gmail.com>
2022-10-11 15:49:24 +0300

    Update wifi_password.py

--- wifi_password.py
+++ wifi_password.py
@@ -2,11 +2,12 @@ import os, re, platform
 from prettytable import PrettyTable
 
 def get_wifi_pass_lin():
-    regx = r'tions\/([\S]+)\..+=([\S]+)'
+    regx = r's\/(.*)\.\S+=(.*)'
     out = os.popen('sudo grep psk= /etc/NetworkManager/system-connections/*').read()
-    table = PrettyTable(['WiFi-Name', 'WiFi-Password'])
-    for name,key in re.findall(regx, out):
-        table.add_row([name, key])
+    table = PrettyTable(['No.','WiFi-Name', 'WiFi-Password'])
+    for idx,(name,key) in enumerate(re.findall(regx, out)):
+        table.add_row([idx+1, name, key])
+      
     return table
 
 def get_wifi_pass_win():
@@ -16,15 +17,15 @@ def get_wifi_pass_win():
     for i in out:
         idx = i.find('\n')
         wifi_name.append(i[:idx].strip())
-    regx = r'Content\s+:\s([\S]+)'
+    regx = r'Content\s+:\s(.*)'
     wifi_pass = []
     for i in wifi_name:
         out = os.popen(f'netsh wlan show profile name= "{i}" key=clear').read()
         wifi_pass.append( re.search(regx, out).group(1) )
 
-    table = PrettyTable(['WiFi-Name', 'WiFi-Password'])
-    for i in range(len(wifi_name)):
-        table.add_row([wifi_name[i], wifi_pass[i]])
+    table = PrettyTable(['No.','WiFi-Name', 'WiFi-Password'])
+    for idx,name in enumerate(wifi_name):
+        table.add_row([idx+1, name, wifi_pass[idx]])
 
     return table
 
